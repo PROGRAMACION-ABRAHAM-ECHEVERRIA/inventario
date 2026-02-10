76218b1e5ccdd574191b54549fdd4451a8e17472
ruben-palma <ruben.palma@dineroinmediato.com.mx>
2026-02-10 10:52:55 -0600

    salidas/ dto editado, y logica mejorada

--- src/app/salidas/salidas.service.ts
+++ src/app/salidas/salidas.service.ts
@@ -19,27 +19,65 @@ export class SalidasService {
     public ApiJson = new resJsonClass();
 
     async salidas(salidasDto: SalidaDTO) {
-        const { cvebod, cveProductos, cvemov, usuarioAlta, usuarioId, fechaSalida, observ, imptot } = salidasDto;
+        const { cvebod, cveProductos, cvemov, usuarioAlta, usuarioId, fechaSalida, observ, imptot, sermov } = salidasDto;
+
+        console.log(sermov, cvebod, cvemov);
+
+        const query = `
+            exec SP_GV_CrearFolMov
+                @cvebod = @0,
+                @cvemov = @1,
+                @sermov = @2;
+        `;
+
+        const resFolMov = await this.dataSource.query(query, [cvebod, cvemov, sermov ?? '']);
+
+        console.log(resFolMov);
+
+        if (resFolMov[0].error || !resFolMov[0].FolMov) {
+            // si falla uno, lanzamos excepción y detenemos el bucle
+            throw this.ApiJson.customeHttpExeption(resFolMov[0].mensaje, resFolMov[0].estatus);
+        }
 
         try {
             // recorrer cada producto
             for (const prod of cveProductos) {
-                const {cant, cveProd} = prod;
+                const { cant, cveProd, impSub, precioUnidad} = prod;
 
                 const query = `
                 exec SP_GV_Salidas
                     @cvebod = @0,
                     @cveprod = @1,
                     @cvemov = @2,
-                    @usuario = @3,
-                    @fechasalida = @4,
-                    @observ = @5,
-                    @imptot = @6,
-                    @usuarioid = @7,
-                    @cantidad = @8;
+                    @folmov = @3,
+                    @sermov = @4,
+                    @usuario = @5,
+                    @fechasalida = @6,
+                    @observ = @7,
+                    @imptot = @8,
+                    @usuarioid = @9,
+                    @cantidad = @10,
+                    @preuni = @11,
+                    @impsub = @12;
                 `;
 
-                const res = await this.dataSource.query(query, [cvebod, cveProd, cvemov, usuarioAlta, fechaSalida, observ, imptot, usuarioId, cant]);
+                const res = await this.dataSource.query(query, [
+                    cvebod, 
+                    cveProd, 
+                    cvemov, 
+                    resFolMov[0].FolMov, 
+                    sermov ?? '', 
+                    usuarioAlta, 
+                    fechaSalida, 
+                    observ, 
+                    imptot, 
+                    usuarioId, 
+                    cant,
+                    precioUnidad,
+                    impSub]
+                );
+
+                console.log(res);
 
                 if (res[0].error) {
                     // si falla uno, lanzamos excepción y detenemos el bucle
